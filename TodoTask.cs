093b1853675dbe3fcb68534df63da7e483613044
Tsvetoslav26 <cvetoslavcvetelinov07@gmail.com>
2026-02-11 12:18:05 +0200

    feat: add task model and core operations

---
+++ TodoTask.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTaskManager
+{
+    internal class TodoTask
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public bool IsCompleted { get; set; }
+
+        public TodoTask(int id, string name, bool completed = false)
+        {
+            Id = id;
+            Name = name;
+            IsCompleted = completed;
+        }
+    }
+}
